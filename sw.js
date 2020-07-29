3fd302bf3ed336b0c4ce5dbc0c2a9473819e1b4f
OscarMiddagh <oscardanie98@gmail.com>
2020-07-29 12:22:11 -0400

    añadiendo manifest

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('fetch', event =>{
     const resp=fetch(event.request)
-                .then(resp=>resp.ok ? resp : fetch('img/kid.jpg'));
-            
+                .then(res=>res); //estamos haciendo cualquier cosa que haga una peticion a traves del sw
+                   
     event.respondWith(resp );
 });
