b2c8aa8f2544dee47fd3b51b9b7e99768ee5ae1a
OscarMiddagh <oscardanie98@gmail.com>
2020-08-03 09:45:38 -0400

    Update sw.js
    Co-Authored-By: jpamonza <46094357+jpamonza@users.noreply.github.com>
    Co-Authored-By: Rodrigo Salguero <33531552+pacha2880@users.noreply.github.com>

--- public/sw.js
+++ public/sw.js
@@ -4,7 +4,7 @@ const CACHE_INMUTABLE_NAME = 'inmutable-v1';
 
 const CACHE_STATIC_FILE =[
                             // '/',
-                            'index.html',
+                            // 'index.html',
                             'img/favicon.ico',
                             'css/style.css',
                             'img/kid.png',
